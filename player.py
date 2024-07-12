37547d3808a390b08671c38c91ef2b8837176eb1
Gustavo Jakobi <gujakobi10@gmail.com>
2024-07-12 19:53:27 -0300

    creating basic packet structure

--- player.py
+++ player.py
@@ -1,14 +1,13 @@
 import socket
 import time
+import json
 
 porta1 = int(input("Digite a porta: "))
 
 # Configurações
 host = 'localhost'
 
-
-porta2 = int(input("Digite a porta do proximo: "))
-
+porta2 = int(input("Digite a porta do próximo: "))
 
 # Criação do socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,14 +16,23 @@ sock.bind(addr1)
 
 proximoJogador = (host, porta2)
 
+# Definir estrutura de pacote, contendo o estado do jogo, a carta jogada, e o destino final
+pacote = {
+    "estado": "Jogando",
+    "carta": None,
+    "destino": None
+}
 
 # Enviar uma mensagem inicial para o próximo jogador
 if porta1 == 5001:
-    msg = input("Jogador, insira uma mensagem para o próximo jogador: ")
+    carta = input("Digite a carta a ser jogada: ")
     sent = False
     while not sent:
         try:
-            sock.sendto(msg.encode(), proximoJogador)
+            # enviar o pacote, com a carta e o destino via JSON
+            pacote["carta"] = carta
+            pacote["destino"] = proximoJogador
+            sock.sendto(json.dumps(pacote).encode(), proximoJogador)
             sent = True
         except socket.error:
             time.sleep(1)
@@ -32,13 +40,18 @@ if porta1 == 5001:
 # Loop principal
 while True:
     data, addr = sock.recvfrom(1024)
-    print(f"{addr} diz: {data.decode()}")
+    pacote = json.loads(data.decode())
 
-    if data.decode() == "Encerrar":
+    # printar carta recebida
+    print("Carta recebida: ", pacote["carta"])
+
+    if pacote["estado"] == "Encerrar":
         break
 
-    # Enviar a mensagem de volta para o jogador 2
-    msg = input("Digite uma mensagem: ")
-    sock.sendto(msg.encode(), proximoJogador)
+    # Enviar a mensagem de volta para o próximo jogador
+    carta = input("Digite a carta: ")
+    pacote["carta"] = carta
+    pacote["destino"] = proximoJogador
+    sock.sendto(json.dumps(pacote).encode(), proximoJogador)
 
 sock.close()
